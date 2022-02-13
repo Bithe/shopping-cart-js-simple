6daf6788456ba6cf1de5954e236bb1683bb3acd0
bithe <farzana.hossain5@gmail.com>
2022-02-13 21:57:11 +0600

    shopping cart

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('I am in a separate file')
+console.log('I am in a separate file');
+document.getElementById('case-plus').addEventListener("click", function() {
+    const caseNumber = document.getElementById('case-number');
+    const caseNumberText = caseNumber.value;
+    caseNumber.value = parseInt(caseNumberText) + 1;
+    console.log(caseNumberText);
+});
