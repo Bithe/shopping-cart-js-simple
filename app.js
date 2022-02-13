7caa2b9d70fc99dbaf60d4fcae6711f7020f6f39
bithe <farzana.hossain5@gmail.com>
2022-02-13 22:23:27 +0600

    increasing and decreasing event

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 console.log('I am in a separate file');
-document.getElementById('case-plus').addEventListener("click", function() {
+
+function getCaseNUmber(isIncreasing) {
     const caseNumber = document.getElementById('case-number');
     const caseNumberText = caseNumber.value;
-    caseNumber.value = parseInt(caseNumberText) + 1;
-    console.log(caseNumberText);
+    if (isIncreasing == true) {
+        caseNumber.value = parseInt(caseNumberText) + 1;
+        console.log(caseNumberText);
+    } else if (caseNumberText > 0) {
+        caseNumber.value = parseInt(caseNumberText) - 1;
+    }
+}
+document.getElementById('case-plus').addEventListener("click", function() {
+    getCaseNUmber(true);
+});
+
+document.getElementById('case-minus').addEventListener("click", function() {
+    getCaseNUmber(false);
 });
